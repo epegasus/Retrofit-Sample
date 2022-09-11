27be198dde0a4a7541c3d3a88b4571e62847ccff
sohaibmughal <sohaibahmed.hypersoft@gmail.com>
2022-09-11 13:31:05 +0500

    Initial Commit

--- app/src/main/java/dev/pegasus/retrofit/retrofit/RetrofitInstance.kt
+++ app/src/main/java/dev/pegasus/retrofit/retrofit/RetrofitInstance.kt
@@ -1,17 +1,27 @@
 package dev.pegasus.retrofit.retrofit
 
 import dev.pegasus.retrofit.interfaces.ClientApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
     val api: ClientApi by lazy {
+        // Add logging interceptor for getting response detail to interprete
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
         Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(ClientApi::class.java)
     }
-
 }
